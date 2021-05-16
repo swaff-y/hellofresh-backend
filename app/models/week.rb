6a2c328179b587e4c7832076c8e0e4a4211ed573
swaff-y <kyle@swaff.id.au>
2021-05-16 11:54:57 +1000

    Week model chaged to show 7 results

--- app/models/week.rb
+++ app/models/week.rb
@@ -2,6 +2,6 @@ class Week < ApplicationRecord
     has_and_belongs_to_many :recipes, association_foreign_key: "recipe_id", join_table: "recipes_weeks"
 
     def total_pages
-      (Week.all.count.to_f / 6).ceil
+      (Week.all.count.to_f / 7).ceil
     end
 end
